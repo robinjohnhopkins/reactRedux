e905fe833981e1a6cf5c75e7c2e725c0d84adb15
Robin Hopkins <robinjohnhopkins@hotmail.com>
2019-10-20 17:38:39 +0100

    Add toast that animates in saying Saved , then animates awayt - slick

--- src/components/courses/ManageCoursePage.js
+++ src/components/courses/ManageCoursePage.js
@@ -5,6 +5,9 @@ import {loadAuthors} from "../../redux/actions/authorActions";
 import PropTypes from "prop-types";
 import CourseForm from "./CourseForm";
 import {newCourse} from "../../../tools/mockData";
+import Spinner from '../common/Spinner';
+import {toast} from 'react-toastify';
+
 
 function ManageCoursePage({
   courses,
@@ -15,12 +18,11 @@ function ManageCoursePage({
   history,
   ...props
 }) {
-  const [course,
-    setCourse] = useState({
+  const [course,setCourse] = useState({
     ...props.course
   });
-  const [errors,
-    setErrors] = useState({});
+  const [errors, setErrors] = useState({});
+  const [saving, setSaving] = useState(false);
 
   useEffect(() => {
     if (courses.length === 0) {
@@ -52,17 +54,25 @@ function ManageCoursePage({
 
   function handleSave(event) {
     event.preventDefault();
+    setSaving(true);
     saveCourse(course).then(() => {
+      toast.success('Course saved');
       history.push("/courses");
     });
   }
 
-  return (<CourseForm
+  return authors.length === 0 || courses.length === 0 ? (
+    <Spinner />
+  ) :
+  (
+  <CourseForm
     course={course}
     errors={errors}
     authors={authors}
     onChange={handleChange}
-    onSave={handleSave}/>);
+    onSave={handleSave}
+    saving={saving}/>
+    );
 }
 
 ManageCoursePage.propTypes = {
